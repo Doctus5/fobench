bd0f39cbe6aeee462f1ed7ffd3fa7befd9e2abc7
Sergio Andres Diaz Meza <doctus5@gmail.com>
2024-07-09 18:25:26 +0200

    functions updated

--- database/manager.py
+++ database/manager.py
@@ -16,6 +16,7 @@ Last modification on 2024-07-08 16:50:00
 # Necessary packages to import
 import os
 import glob
+import pandas as pd
 
 # inside packaged
 from ..fobench.fiber import Fiber
@@ -52,12 +53,32 @@ def chrono_order(files, company):
 	'''
 
     N = len(files) # number of data files.
+    filtered_keys = ['start_time', 'end_time', 'dt', 'sampling_frequency', 'total_channels', 
+                     'spatial_interval', 'gauge_lenght', 'channel_offset'] # attributes of interest for holding consistency.
+    #filtered_keys = ['start_time', 'end_time', 'dt'] # attributes of interest for holding consistency.
     
-    for i, file in enumerate(files):
+    database = []
     
-        d_file = Fiber(file, company=company, load_data=True)
-        file_start, file_end = d_file.start_time, d_file.end_time
+    # scan the files
+    for i in range(N):
+    
+        file = files[i] # select file
+        # by checking the start and end times in the files
+        d_file = Fiber(file, company=company, load_data=False)
         
-        print(i, file_start, file_end)
+        info = d_file.metadata(meta_dict=True) # getting public relevant attributes.
+        info = {key: info[key] for key in filtered_keys if key in info}  # Filter
+        info['file'] = d_file.__filepath__ # we also take the filepath.
+        
+        # correct format for start and end times into strings (UTC also can do the thing).
+        info['start_time'] = info['start_time'].isoformat()
+        info['end_time'] = info['end_time'].isoformat()
+        
+        database.append(info)
+        del d_file # free memory.
+        
+    # handling it as a DataFrame
+    info = pd.DataFrame.from_dict(database)
+    info = info.sort_values(by=['start_time', 'end_time'])
     
-    #return 0
+    return 0
