d626ea3ba0b51083126f004522a65f9c76e453a7
Jonas Pätzel <jonas.patzel@ulb.be>
2024-07-15 19:34:46 +0200

    added docs to _update_processing

--- fobench/tools.py
+++ fobench/tools.py
@@ -49,42 +49,53 @@ TEMP_UNIT_MAP = {
 
 
 def _update_processing(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        
-        func_name = func.__name__
-        
-        # extract all arguments
-        bound_arguments = inspect.signature(func).bind(*args, **kwargs)
-        bound_arguments.apply_defaults()
-        args_dict = bound_arguments.arguments
-        args_dict.pop('self')
-        
-        # function call
-        result = func(*args, **kwargs)
-        
-        # append info to fiber instance
-        fiber = args[0]
-        fiber.processing.append({func_name : args_dict})
-        
-        if func_name in ['integrate', 'differentiate']:
-            if (fiber.sensing == 'das' or fiber.sensing == 'dss'): unit_map = STRAIN_UNIT_MAP
-            elif fiber.sensing == 'dts': unit_map = TEMP_UNIT_MAP
-            
-            # find current unit
-            try: key = [i for i in unit_map if unit_map[i] == fiber.units][0]
-            except: key = fiber.units[2]
+	'''
+	Co-authors: Jonas Pätzel
+	Description: 
+		internal decorator function that updates the Fiber.processing attribute after each processing function
+		in case of integration or differentiation of the data also updates Fiber.units
+	:Params:
+		-func(type: function): preprocessing function that will be logged
+	:Return:
+		- NA
+	'''
 
-            	# depending on operation change key
-            if func_name == 'integrate': key -= 1
-            elif func_name == 'differentiate': key += 1
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		
+		func_name = func.__name__
+		
+		# extract all arguments
+		bound_arguments = inspect.signature(func).bind(*args, **kwargs)
+		bound_arguments.apply_defaults()
+		args_dict = bound_arguments.arguments
+		args_dict.pop('self')
+		
+		# function call
+		result = func(*args, **kwargs)
+		
+		# append info to fiber instance
+		fiber = args[0]
+		fiber.processing.append({func_name : args_dict})
+		
+		if func_name in ['integrate', 'differentiate']:
+			if (fiber.sensing == 'das' or fiber.sensing == 'dss'): unit_map = STRAIN_UNIT_MAP
+			elif fiber.sensing == 'dts': unit_map = TEMP_UNIT_MAP
+			
+			# find current unit
+			try: key = [i for i in unit_map if unit_map[i] == fiber.units][0]
+			except: key = fiber.units[2]
 
-            	# assign new unit
-            try: fiber.units = unit_map[key]
-            except: fiber.units = f'd^{key}/dt {unit_map[0]} [dm/m]'
-            
-        return result
-    return wrapper
+				# depending on operation change key
+			if func_name == 'integrate': key -= 1
+			elif func_name == 'differentiate': key += 1
+
+				# assign new unit
+			try: fiber.units = unit_map[key]
+			except: fiber.units = f'd^{key}/dt {unit_map[0]} [dm/m]'
+			
+		return result
+	return wrapper
 
 
 
