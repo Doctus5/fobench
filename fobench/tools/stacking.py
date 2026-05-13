e7f269afa5bf22236273fe3b8aac5897c63cbade
Jonas <jonas.patzel@ulb.be>
2026-05-13 13:20:38 +0200

    fixed stacking docstring to avoid import error

--- fobench/tools/stacking.py
+++ fobench/tools/stacking.py
@@ -6,67 +6,74 @@ import numpy as np
 from scipy.signal import hilbert
 from obspy.signal.util import stack
 
-def stack_2D(data: np.ndarray, stack_type: str | tuple(str, int)) -> np.ndarray:
+def stack_2D(data: np.ndarray, stack_type: str | tuple[str, int]) -> np.ndarray:
     '''
-    stacks data given as 2D array with dimensions (n_signals, n_samples)
-    calls obspy.signal.util.stack
-    e.g. for stacking channel data
+    Stacks data given as 2D array with dimensions ``(n_signals, n_samples)``.
+    Calls ``obspy.signal.util.stack``. Ideal for stacking channel data
 
     Parameters
     ----------
     data : np.ndarray
-        data to stack.
-    stack_type : str | tuple(str, int)
-        stack type,options are:
-            -'linear'
-            -('pw', order)
-            -('root', order)
+        Data to stack.
+    stack_type : str | tuple[str, int]
+        Stack type, options are:
+            - ``'linear'``
+            - ``('pw', order)``
+            - ``('root', order)``
         linear stack refers to mean stack, not sum!
 
     Returns
     -------
     np.ndarray
-        stack data.
+        Stacked data.
+
+    See also
+    --------
+    `obspy.signal.util.stack <https://docs.obspy.org/_modules/obspy/signal/util.html#stack>`_
+
     '''
 
     return stack(data, stack_type)
 
-
-
-
-def stack_3D(data: np.ndarray, stack_type: str | tuple(str, int))-> np.ndarray:
+def stack_3D(data: np.ndarray, stack_type: str | tuple[str, int])-> np.ndarray:
     '''
-    Stacks data given as a 3D array (n_signals, n_samples, n_windows).
-    Implements linear (mean), sum, and phase-weighted stacking (PWS).
+    Stacks data given as a 3D array ``(n_signals, n_samples, n_windows)``.
+    Implements linear (mean), sum, and phase-weighted stacking (PWS). Adapted
+    from ``obspy.signal.util.stack``.
 
     Parameters
     ----------
     data : np.ndarray
         3D array (n_signals, n_samples, n_windows).
-    stack_type : str | tuple(str, int)
-        type of stack, options are:
-            -'linear': mean stack
-            -'sum': summation stack
-            -('pw', order): phase-weighted stack with given order
+    stack_type : str | tuple[str, int]
+        Type of stack, options are:
+            - 'linear': mean stack
+            - 'sum': summation stack
+            - ('pw', order): phase-weighted stack with given order
 
     Raises
     ------
     ValueError
-        invalid stack_type given.
+        Invalid stack_type given.
 
     Returns
     -------
     np.ndarray
-        stacked data.
+        Stacked data.
+
+    See also
+    --------
+    `obspy.signal.util.stack <https://docs.obspy.org/_modules/obspy/signal/util.html#stack>`_
+
     '''
 
-    if stack_type == 'linear':
+    if stack_type == "linear":
         return np.mean(data, axis=2)
 
-    elif stack_type == 'sum':
+    elif stack_type == "sum":
         return np.sum(data, axis=2)
 
-    elif isinstance(stack_type, tuple) and stack_type[0] == 'pw':
+    elif isinstance(stack_type, tuple) and stack_type[0] == "pw":
         order = stack_type[1]
         n_signals, n_samples, n_windows = data.shape
 
@@ -85,6 +92,5 @@ def stack_3D(data: np.ndarray, stack_type: str | tuple(str, int))-> np.ndarray:
         return linear_stack * phase_stack
     else:
         raise ValueError(
-            'Please provide a stacking method of either "linear", ("pw", order")'
-            'or "sum'
-        )
+            "Please provide a stacking method of either 'linear', ('pw', order)"
+            "or 'sum'")
